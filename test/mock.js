035a5d4f0f5cdcff5852a60cb94d4df6dcf4f9d2
Fei He <fei.he@houzz.com>
2017-05-30 11:22:52 -0700

    adding mock support

---
+++ test/mock.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+    data: {
+        photos: {
+            items: [
+                {
+                    id: '1',
+                    title: 'backyard lounge',
+                    url: 'https://st.hzcdn.com/fimgs/1691882808e1753e_6226-w300-h300-b0-p0--traditional-porch.jpg'
+                },
+                {
+                    id: '2',
+                    title: 'traditional kitchen',
+                    url: 'https://st.hzcdn.com/fimgs/38110dd308d95e45_0873-w300-h300-b0-p0--traditional-kitchen.jpg'
+                },
+                {
+                    id: '3',
+                    title: 'rustic kitchen',
+                    url: 'https://st.hzcdn.com/fimgs/8811746408ebb5db_5220-w300-h300-b0-p0--rustic-kitchen.jpg'
+                },
+                {
+                    id: '4',
+                    title: 'contemporary bathroom',
+                    url: 'https://st.hzcdn.com/fimgs/69510bcc08bfa031_2698-w300-h300-b0-p0--contemporary-bathroom.jpg'
+                }
+            ]
+        }
+    }
+};
